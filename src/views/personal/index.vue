c8ecd1ddbf0a4cfaa727b9a5f1aacb69b4dea819
rain-bow02 <rain-bow02@qq.com>
2023-06-12 21:38:45 +0800

    Merge branch 'main' of https://github.com/natunev/work

--- src/views/personal/index.vue
+++ src/views/personal/index.vue
@@ -1,23 +1,14 @@
 <template>
-    <div>
-    
-    </div>
+  <div>hello</div>
 </template>
 <script>
-
-export default{
-    name:'Personal', //组件名称
-    data(){
-        return{
-
-        }
-    },
-    methods:{
-
-    },
-}
+export default {
+  name: "Personal", //组件名称
+  data() {
+    return {};
+  },
+  methods: {},
+};
 </script>
 <!-- scoped: 作用域，当前css只当前的组件生效-->
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
